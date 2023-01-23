099039ec3dea811d20b8afbdc1f16630f1acd504
cnsj <chanutgg@gmail.com>
2023-01-23 12:30:09 +0700

    ีupdate location

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "location" {
   description = "The Azure Region in which all resources groups should be created."
   type        = string
-  default = "eastus"
+  default = "southeastasia"
 }
 variable "resource_group_name" {
   description = "The name of the resource group"
