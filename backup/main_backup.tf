11fd8383797dffa464c78c348193320889921a88
cnsj <chanutgg@gmail.com>
2023-01-23 14:45:08 +0700

    modules test

--- backup/main_backup.tf
+++ backup/main_backup.tf
@@ -11,8 +11,19 @@ resource "random_string" "random" {
   lower = true
   
 }
-module "azure_network" {
-  source              = "./network" 
+resource "azurerm_virtual_network" "example" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subnetname"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
 }
 
 resource "azurerm_storage_account" "example" {
