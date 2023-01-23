bcd0b1d834865ff48ea34f68c305b6c99f4199c7
cnsj <chanutgg@gmail.com>
2023-01-23 15:16:29 +0700

    again2

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_storage_account" "example" {
   network_rules {
     default_action             = "Deny"
     ip_rules                   = ["100.0.0.1"]
-    virtual_network_subnet_ids = [module.azure_network.id]
+    virtual_network_subnet_ids = [module.azure_network.subnet_id]
   }
 
   tags = {
