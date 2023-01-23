b306a34e5882b49392eb69179d33815d8060d0fa
cnsj <chanutgg@gmail.com>
2023-01-23 16:21:14 +0700

    test11

--- network/network.tf
+++ network/network.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "example" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = network_location
-  resource_group_name = network_resource_group_name
+  location            = var.network_location
+  resource_group_name = var.network_resource_group_name
 }
 
 resource "azurerm_subnet" "example" {
