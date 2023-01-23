eda05f70ee5c5fa8608ae8eab2e895ee41f024af
cnsj <chanutgg@gmail.com>
2023-01-23 16:49:06 +0700

    38

--- network/network.tf
+++ network/network.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = var.network_resource_group_name
+  location = var.network_location
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.network_location
-  resource_group_name = var.network_resource_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "testsubnet"
-  resource_group_name  = var.network_resource_group_name
+  resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = ["10.0.3.0/24"]
   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
